9a9b3a987ef11e0f28ec207cc9f37bd42b21f4d8
weverton alves <wevertonm22@iclou.com>
2024-11-28 19:15:22 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,8 @@
+function alterarStatus(id) {
+    let fameClicado = document.getElementById(`game-${id}`);
+    let imagen = gameClicadado.querySelector('.dashboard__item__img');
+    let botao = gameClicadado.querySelector('.dashboard__item__button');
+    let nomeJogo = gameClicadado.querySelector('.dashboard__item__name');
+    
+    alert(nomeJogo.textcontent);
+}
