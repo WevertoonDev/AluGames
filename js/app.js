1d2685be7e3d85967b39635cfc0ff15681e65221
weverton alves <wevertonm22@iclou.com>
2024-11-30 20:20:48 -0300

    Alterando status do Alugames

--- js/app.js
+++ js/app.js
@@ -2,6 +2,14 @@ function alterarStatus(id) {
     let gameClicado = document.getElementById(`game-${id}`);
     let imagen = gameClicado.querySelector('.dashboard__item__img');
     let botao = gameClicado.querySelector('.dashboard__item__button');
-    let nomeJogo = gameClicado.querySelector('.dashboard__item__name');
     
+    if (imagen.classList.contains('dashboard__item__img--rented')) {
+        imagen.classList.remove('dashboard__item__img--rented');
+        botao.classList.remove('dashboard__item__button--return');
+        botao.textContent = 'Alugar';
+    }    else {
+        imagen.classList.add('dashboard__item__img--rented');
+        botao.textContent = 'Devolver';
+        botao.classList.add('dashboard__item__button--return');
+    }
 }
